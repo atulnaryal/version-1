c0afec8fd1b9aa5a18003ba1cc3b31ea013a30a7
atulnaryal <atulnaryal777@gmail.com>
2025-03-12 16:13:36 +0000

    datatypes

---
+++ 01_basics/01_veriables.js
@@ -0,0 +1,25 @@
+const accountId = 144553
+let accountEmail = "atunaryal777@gmail.com"
+var accountPassword = "123456"
+accountCity = "jaipur" 
+let accountState;
+
+
+// accountId = 2
+
+/*
+prefer not to use var 
+because of issue in block scope and functional scope
+
+*/
+
+accountEmail = "chushus9g9"
+accountPassword = 21552
+accountCity = "bamjgyfy"
+
+
+console.log(accountEmail);
+console.table([accountId, accountEmail, accountPassword, accountPassword, accountState])
+
+
+
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code as newer version 
+
+//alert(hello) // we are using node js not browser
+//alert(3+3) // we are using node js not browser
+// console.log(3+3)
+// console.log('atul') code readability should be high
+
+let name="atul"
+let age= 25
+let isLoggedIn= false
+
+
+
+// number==> 2 to power 53
+// bigint 
+// string==> ""
+//boolean => true/false
+//null ==> standalone value
+// undefined =>
+    //symbol => unique
+
+//object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); //object
